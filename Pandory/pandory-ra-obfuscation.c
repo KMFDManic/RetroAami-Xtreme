dc4eda00936ae9ffff320c3fcde0e6d38aac8f5d
Kyland K <kkainbryce@oh.rr.com>
2022-10-07 18:26:41 -0400

    Remove Obfuscated RetroArch Segmentation Fault
    Anomaly Detected with seemingly intentional Obfuscation, rendering Pandory useless, if one attempts to move RetroArch outside its home environment.  Being that this is clear Open Source License Breach of RetroArch by whomever originally coded it in (possible oversight?) it obviously had to be removed!
    
    capsimg.so is also not permissible to be included in any Commercial Project,  along with other files.  License dictates it can only be by "itself".  Removed from Pandory Source to abide by original License Agreement.
    
    Additionally, checks for "Dummy" file, libmali.so, as well as the .pandory 200+ MB size check have also been eliminated, as they serve no realistic or practical purpose.
    
    Unneeded Libraries and Binary Files Excised.

---
+++ Pandory/pandory-ra-obfuscation.c
@@ -0,0 +1,46 @@
+Hex Edit Removed Segmentation Fault Obfuscation Oversight to correct License Violation:
+
+int main(int argc,char **argv)
+{
+  size_t len;
+  int ret;
+  stat64 statbuf;
+  char pathname [80];
+  char cmdreboot [8];
+  int fileExists;
+
+  memcpy(cmdreboot,"reb",4);
+  len = strlen(cmdreboot);
+  memcpy(cmdreboot + len,"oot",4);
+
+  memcpy(pathname,"/mn",4);
+  len = strlen(pathname);
+  memcpy(pathname + len,"t/Pan",6);
+  len = strlen(pathname);
+  memcpy(pathname + len,"dor",4);
+  len = strlen(pathname);
+  memcpy(pathname + len,"y/.pan",7);
+  len = strlen(pathname);
+  memcpy(pathname + len,"do",3);
+  len = strlen(pathname);
+  memcpy(pathname + len,"ry",3);
+
+  fileExists = false;
+  ret = access(pathname,F_OK);
+  if (ret == 0) {
+    stat(pathname,&statbuf);
+    if (statbuf.st_size > 200000000) {
+      fileExists = true;
+    }
+  }
+
+  if (fileExists) {
+    ret = rarch_main(argc,argv,0);
+  }
+  else {
+    printf("Segmentation fault");
+    system(cmdreboot);
+    ret = 53;
+  }
+  return ret;
+}
